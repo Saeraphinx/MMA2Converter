d1a7cd066cd5ce951a923580c4c26f9c1165113c
TM0D <tmodder24@gmail.com>
2021-02-04 00:52:22 -0600

    javapoint.com is annoying

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
-//  This will be the worst code you have ever seen. Sorry but I don't care.
-import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import java.io.*; 
 
+//  This will be the worst code you have ever seen. Sorry but I don't care.
 public class Main {
     private static Converter converter;
     private static JFrame frame;
@@ -11,8 +12,20 @@ public class Main {
         frame.setLayout(null);
 
         // Create path selector
-        JButton fileOpen = new JButton("Open level.dat");
-        fileOpen.setBounds(50,100,50,100);
+        JButton fileRead = new JButton("Open level.dat");
+        JButton fileOut = new JButton("Set Output");
+        fileRead.setBounds(5,5,120,20);
+        fileOut.setBounds(130,5,120,20);
+
+        fileRead.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e) {      
+                String name = JOptionPane.showInputDialog(frame,"Enter Name");  
+            }  
+        });  
+
+
+        frame.add(fileRead);
+        frame.add(fileOut);
          
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
